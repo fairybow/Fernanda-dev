19d44e0cab6e10ec45cd2f7bc0438088c3428963
jackson <63016612+fairybow@users.noreply.github.com>
2023-04-28 00:28:44 -0400

    :tropical_drink:

--- Fernanda/source/menu/HtmlString.hpp
+++ Fernanda/source/menu/HtmlString.hpp
@@ -16,6 +16,12 @@ inline QString operator%(const char* lhs, const QString& rhs) { return QString(l
 inline QString operator%(const QString& lhs, const std::string& rhs) { return lhs % QString::fromStdString(rhs); }
 inline QString operator%(const std::string& lhs, const QString& rhs) { return QString::fromStdString(lhs) % rhs; }
 
+inline QString operator%=(QString& lhs, const QString& rhs)
+{
+	lhs = lhs % rhs;
+	return lhs;
+}
+
 inline QString operator/(const QString& lhs, const QString& rhs)
 {
 	return lhs + QStringLiteral("<br>") + rhs;
@@ -29,21 +35,21 @@ namespace HtmlString
 {
 	namespace StdFs = std::filesystem;
 
-	namespace {
-		inline QString multiply(const char* character, int defaultArgument = 1)
-		{
-			if (defaultArgument < 1)
-				defaultArgument = 1;
-			QString result;
-			if (character[1] == '\0')
-				result = QString(defaultArgument, character[0]);
-			else {
-				for (int i = 0; i < defaultArgument; ++i)
-					result += character;
-			}
-			return result;
+	inline QString multiply(const char* character, int defaultArgument = 1)
+	{
+		if (defaultArgument < 1)
+			defaultArgument = 1;
+		QString result;
+		if (character[1] == '\0')
+			result = QString(defaultArgument, character[0]);
+		else {
+			for (int i = 0; i < defaultArgument; ++i)
+				result += character;
 		}
+		return result;
+	}
 
+	namespace {
 		inline QString tableColumnSpacing(int columns = 9)
 		{
 			return multiply("<td>\n</td>", columns);
@@ -76,16 +82,15 @@ namespace HtmlString
 		return QString("<h%1><b>%2</b></h%1>").arg(level).arg(text);
 	}
 
-	template<typename T, typename U>
-	inline QString link(const T& url, U displayName = U())
+	template<typename T>
+	inline QString link(const T& url, QString displayName = QString())
 	{
 		if (displayName.empty())
-			displayName = U(url).replace(std::regex("(https:\\/\\/|www.)"), "");
+			displayName = QString(url).replace(std::regex("(https:\\/\\/|www.)"), "");
 		return QString("<a href='%1'>%2</a>").arg(url).arg(displayName);
 	}
 
-	template<typename T>
-	inline QString link(const StdFs::path& url, T displayName = T())
+	inline QString link(const StdFs::path& url, QString displayName = QString())
 	{
 		return link(url.generic_string(), displayName);
 	}
--- Fernanda/source/menu/VersionChecker.hpp
+++ Fernanda/source/menu/VersionChecker.hpp
@@ -3,25 +3,73 @@
 #include "../Version.hpp"
 #include "HtmlString.hpp"
 
+#include <QEventLoop>
 #include <QJsonDocument>
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
+#include <QProgressDialog>
+#include <QVariantMap>
 
 class VersionChecker : public QObject
 {
 	Q_OBJECT
 
 public:
-	static inline const QString check(QWidget* parent)
+	static inline const QString check(const QUrl& gitHubReleasesApi, const QUrl& gitHubReleases, QWidget* parent)
 	{
 		QString text = {
 			HtmlString::heading("Version") %
 			HtmlString::bold("Current version:") /
 			VER_FILEVERSION_STR
 		};
-
-		// etc.
-
+		auto map = latestVersion(gitHubReleasesApi, parent);
+		if (!map.isEmpty()) {
+			auto latest = map["tag_name"].toString();
+			if (latest == QString(VER_FILEVERSION_STR))
+				text += QStringLiteral("You have the latest version.");
+			else {
+				QString message = {
+					HtmlString::bold("New version:") /
+					latest %
+					QStringLiteral("You do not have the latest version.") %
+					HtmlString::bold("Download:") /
+					HtmlString::link(gitHubReleases)
+				};
+				text %= message;
+			}
+		}
+		else {
+			QString message = {
+				QStringLiteral("Unable to verify version.") %
+				HtmlString::bold("Check:") /
+				HtmlString::link(gitHubReleases)
+			};
+			text %= message;
+		}
 		return text;
+	}
+
+private:
+	static inline QVariantMap latestVersion(const QUrl& url, QWidget* parent)
+	{
+		QEventLoop loop;
+		auto manager = new QNetworkAccessManager(parent);
+		auto request = QNetworkRequest(url);
+		auto reply = manager->get(request);
+		connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
+		QProgressDialog progress_dialog("Checking...", nullptr, 0, 0, parent);
+		progress_dialog.setWindowModality(Qt::WindowModal);
+		progress_dialog.show();
+		loop.exec();
+		progress_dialog.hide();
+		QVariantMap map;
+		if (reply->error() == QNetworkReply::NoError) {
+			auto json_document = QJsonDocument::fromJson(reply->readAll());
+			auto variant_list = json_document.toVariant().toList();
+			map = variant_list[0].toMap();
+		}
+		reply->deleteLater();
+		manager->deleteLater();
+		return map;
 	}
 };
