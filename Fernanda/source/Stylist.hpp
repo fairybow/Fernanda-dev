394eb67ef67073768265ed87229152dc3e262822
jackson <63016612+fairybow@users.noreply.github.com>
2023-04-29 09:53:05 -0400

    Update Stylist.hpp

--- Fernanda/source/Stylist.hpp
+++ Fernanda/source/Stylist.hpp
@@ -12,8 +12,8 @@ class Stylist : public QObject
 	using StdFsPath = std::filesystem::path;
 
 public:
-	inline Stylist(QVector<QWidget*> widgets, QObject* parent = nullptr)
-		: QObject(parent)
+	inline Stylist(QVector<QWidget*> widgets, QObject* parent = nullptr, const QString& baseSuffix = "Base", const QString& extension = ".qss")
+		: QObject(parent), m_baseSuffix(baseSuffix), m_extension(extension)
 	{
 		for (auto& widget : widgets)
 			add(widget);
@@ -34,14 +34,16 @@ private:
 		bool hasTheme = true;
 	};
 
+	const QString& m_baseSuffix;
+	const QString& m_extension;
 	QVector<Stylee> m_stylees;
 
 	inline QPair<StdFsPath, StdFsPath> sheetPathsFromClass(QWidget* widget)
 	{
 		QString class_name = widget->metaObject()->className();
 		auto root = Path::toStdFs(":/stylist");
-		auto base_filename = class_name + "Base.qss";
-		auto style_filename = class_name + ".qss";
+		auto base_filename = class_name + m_baseSuffix + m_extension;
+		auto style_filename = class_name + m_extension;
 		auto base_path = root / base_filename.toStdString();
 		auto style_path = root / style_filename.toStdString();
 		return qMakePair(base_path, style_path);
