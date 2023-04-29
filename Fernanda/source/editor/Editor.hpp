12567dab2bf9bdce7a6af37051d780b0d9d9f67b
jackson <63016612+fairybow@users.noreply.github.com>
2023-04-29 14:48:28 -0400

    Update Editor.hpp

--- Fernanda/source/editor/Editor.hpp
+++ Fernanda/source/editor/Editor.hpp
@@ -4,6 +4,7 @@
 #include "TrueEditor.h"
 
 #include <QLabel>
+#include <QPushButton>
 #include <QVector>
 
 class Editor : public QWidget
@@ -16,9 +17,17 @@ public:
 	{
 		m_trueEditor->setObjectName(name);
 		m_lineNumberArea->setObjectName(name + QString("-line-number-area"));
+		m_trueEditor->setReadOnly(true);
 		m_trueEditor->setLineNumberArea(m_lineNumberArea);
+		m_trueEditor->viewport()->setCursor(Qt::ArrowCursor);
+		buildScrollBar();
 		Layout::stack(this, { m_shadow, m_overlay, m_trueEditor, m_underlay });
 	}
+
+	// funnel to and from QPTE
+	void setReadOnly(bool readOnly) { m_trueEditor->setReadOnly(readOnly); }
+	bool isReadOnly() const { return m_trueEditor->isReadOnly(); }
+	// add when needed
 
 private:
 	TrueEditor* m_trueEditor = new TrueEditor(this);
@@ -26,4 +35,29 @@ private:
 	QLabel* m_shadow = new QLabel(this);
 	QLabel* m_overlay = new QLabel(this);
 	QLabel* m_underlay = new QLabel(this);
+	QPushButton* m_scrollPrevious = new QPushButton;
+	QPushButton* m_scrollUp = new QPushButton;
+	QPushButton* m_scrollDown = new QPushButton;
+	QPushButton* m_scrollNext = new QPushButton;
+
+	void buildScrollBar()
+	{
+		m_trueEditor->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+		m_trueEditor->addScrollBarWidget(m_scrollUp, Qt::AlignTop);
+		m_trueEditor->addScrollBarWidget(m_scrollPrevious, Qt::AlignTop);
+		m_trueEditor->addScrollBarWidget(m_scrollNext, Qt::AlignBottom);
+		m_trueEditor->addScrollBarWidget(m_scrollDown, Qt::AlignBottom);
+		for (const auto& button : { m_scrollUp, m_scrollDown }) {
+			button->setAutoRepeat(true);
+			button->setAutoRepeatDelay(500);
+		}
+		auto up_arrow = "\U000025B2";
+		auto down_arrow = "\U000025BC";
+		m_scrollUp->setText(up_arrow);
+		m_scrollPrevious->setText(up_arrow);
+		m_scrollNext->setText(down_arrow);
+		m_scrollDown->setText(down_arrow);
+		for (const auto& button : { m_scrollUp, m_scrollPrevious, m_scrollNext, m_scrollDown })
+			button->setMinimumHeight(30);
+	}
 };
