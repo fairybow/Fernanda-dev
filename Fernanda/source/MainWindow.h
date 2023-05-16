7bfb52ca9de78fd14cf118047c3ce70646baa4c1
jackson <63016612+fairybow@users.noreply.github.com>
2023-05-16 18:00:21 -0400

    :yum:

--- Fernanda/source/MainWindow.h
+++ Fernanda/source/MainWindow.h
@@ -7,6 +7,7 @@
 #include "editor/Editor.h"
 #include "menu/MenuBar.h"
 #include "preview/Preview.hpp"
+#include "tab/TabBar.h"
 #include "tools/AlwaysOnTop.hpp"
 #include "tools/StayAwake.hpp"
 #include "tools/PomodoroTimer.hpp"
@@ -16,7 +17,6 @@
 #include "Meter.h"
 #include "StatusBar.hpp"
 #include "Stylist.h"
-#include "TabBar.h"
 
 #include <QCloseEvent>
 #include <QMainWindow>
