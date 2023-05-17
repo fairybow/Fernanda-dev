07670e74fe12f15218442860a6cf5068bec724cb
jackson <63016612+fairybow@users.noreply.github.com>
2023-05-17 16:21:30 -0400

    :loud_sound:

--- Fernanda/source/tab/TrueTabBar.hpp
+++ Fernanda/source/tab/TrueTabBar.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "../common/Utility.hpp"
+
+#include <QMouseEvent>
 #include <QTabBar>
+#include <QToolButton>
 
 class TrueTabBar : public QTabBar
 {
@@ -10,6 +14,55 @@ public:
 	TrueTabBar(QWidget* parent = nullptr)
 		: QTabBar(parent)
 	{
-		//
+		setAutoHide(false);
+		setTabsClosable(true);
+		setMovable(true);
+		setExpanding(false);
+		setUsesScrollButtons(true);
+	}
+
+signals:
+	void mousePressed();
+	void mouseMoved();
+	void mouseReleased();
+	void resized();
+	void inserted();
+	void removed();
+
+protected:
+	virtual void mousePressEvent(QMouseEvent* event) override
+	{
+		QTabBar::mousePressEvent(event);
+		emit mousePressed();
+	}
+
+	virtual void mouseMoveEvent(QMouseEvent* event) override
+	{
+		QTabBar::mouseMoveEvent(event);
+		emit mouseMoved();
+	}
+
+	virtual void mouseReleaseEvent(QMouseEvent* event) override
+	{
+		QTabBar::mouseReleaseEvent(event);
+		emit mouseReleased();
+	}
+
+	virtual void resizeEvent(QResizeEvent* event) override
+	{
+		QTabBar::resizeEvent(event);
+		emit resized();
+	}
+
+	virtual void tabInserted(int index) override
+	{
+		QTabBar::tabInserted(index);
+		emit inserted();
+	}
+
+	virtual void tabRemoved(int index) override
+	{
+		QTabBar::tabRemoved(index);
+		emit removed();
 	}
 };
