2a261ba6b1be827cdf1ada7466998ef4af30fe8c
jackson <63016612+fairybow@users.noreply.github.com>
2023-05-23 18:11:48 -0400

    :beetle:

--- Fernanda/unused/LaunchCop.hpp
+++ Fernanda/unused/LaunchCop.hpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
 ```
 */
 
-#pragma once
+/*#pragma once
 
 #include <QApplication>
 #include <QLocalServer>
@@ -100,4 +100,4 @@ private slots:
 
 		main_window->activateWindow();
 	}
-};
+};*/
