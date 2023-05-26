4d43536b0634c4853cb754bbf905a628cdb82db1
jackson <63016612+fairybow@users.noreply.github.com>
2023-05-25 20:26:20 -0400

    Update Logger.hpp

--- Fernanda/source/Logger.hpp
+++ Fernanda/source/Logger.hpp
@@ -21,16 +21,16 @@ namespace Logger
 	namespace
 	{
 		static bool isInitialized = false;
-		static QFile outFile;
-		static QTextStream stream(&outFile);
+		static QFile log;
+		static QTextStream logStream(&log);
 
 		inline void toConsole(const QString& message)
 		{
 
 #ifdef Q_OS_WINDOWS
 
-			auto wstr = message.toStdWString();
-			OutputDebugStringW(wstr.c_str());
+			auto wstring = message.toStdWString();
+			OutputDebugStringW(wstring.c_str());
 
 #endif
 
@@ -39,10 +39,10 @@ namespace Logger
 
 		inline void clearOnFirstWrite()
 		{
-			outFile.close();
-			outFile.open(QIODevice::WriteOnly);
-			outFile.close();
-			outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+			log.close();
+			log.open(QIODevice::WriteOnly);
+			log.close();
+			log.open(QIODevice::WriteOnly | QIODevice::Append);
 			isInitialized = true;
 		}
 
@@ -52,31 +52,31 @@ namespace Logger
 			auto timestamp = StringTools::time();
 
 			if (last_timestamp != timestamp || force) {
-				stream << timestamp
+				logStream << timestamp
 					<< "\n" << Qt::endl;
 				last_timestamp = timestamp;
 			}
 		}
 
-		inline void log(const QString& message)
+		inline void toLog(const QString& message)
 		{
 			if (!isInitialized)
 				clearOnFirstWrite();
 			timestamp();
-			stream << StringTools::clean(message) << "\n" << Qt::endl;
+			logStream << StringTools::clean(message) << "\n" << Qt::endl;
 		}
 
 		inline void passthrough(QtMsgType type, const QMessageLogContext& context, const QString& message)
 		{
 			toConsole(message);
-			log(message);
+			toLog(message);
 		}
 	}
 
 	void install(const StdFsPath& path)
 	{
-		outFile.setFileName(path / "Debug.log");
-		outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+		log.setFileName(path / "Debug.log");
+		log.open(QIODevice::WriteOnly | QIODevice::Append);
 		qInstallMessageHandler(passthrough);
 	}
 }
